3c884709a9a1e95429ba9edf16e745f80dcc141f
Hugo Avril <hugo.avril@outlook.fr>
2022-02-15 23:42:25 +0100

    CSS display flex -> display grid

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const fs = require('fs');
 const app = express();
 const port = 3030;
 
+// fonction pour raccourcir le dernier mot d'une cellule afin d'éviter 
+// qu'elle se colle au tableau (padding/margin droit non possible)
+handlebars.registerHelper("maxCellChar", function (string) {
+  let liste = string.split(' ');
+  let lastIndex = liste.length - 1;
+
+  if (liste[lastIndex].length > 18)
+    liste.splice(lastIndex, 1, liste[lastIndex].slice(0, 18).concat("..."));
+
+  return liste.join(" ");
+});
+
 const source = fs.readFileSync('./index.hbs').toString();
 const template = handlebars.compile(source);
 const data = JSON.parse(fs.readFileSync('./lists.json'));
