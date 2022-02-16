340405698d0d86001b9c4c2998a75924feb1119c
Hugo Avril <hugo.avril@outlook.fr>
2022-02-16 18:26:45 +0100

    modifs + nettoyage

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const handlebars = require('handlebars');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3030;
 
 // fonction pour raccourcir le dernier mot d'une cellule afin d'éviter 
 // qu'elle se colle au tableau (padding/margin droit non possible)
+// TODO: amélio, splice/slice tous les mots, verif perfs
 handlebars.registerHelper("maxCellChar", function (string) {
   let liste = string.split(' ');
   let lastIndex = liste.length - 1;
@@ -16,9 +18,9 @@ handlebars.registerHelper("maxCellChar", function (string) {
   return liste.join(" ");
 });
 
-const source = fs.readFileSync('./index.hbs').toString();
+const source = fs.readFileSync(path.resolve(__dirname, 'index.hbs')).toString();
 const template = handlebars.compile(source);
-const data = JSON.parse(fs.readFileSync('./lists.json'));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'lists.json')));
 const result = template(data);
 
 app.get('/', (req, res) => {
