60752f9dae068d4800c547829ab8a3b557932398
Hugo Avril <hugo.avril@outlook.fr>
2022-02-16 22:38:59 +0100

    refonte json + href libelle categ + amélio CSS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3030;
 
 // fonction pour raccourcir le dernier mot d'une cellule afin d'éviter 
 // qu'elle se colle au tableau (padding/margin droit non possible)
-// TODO: amélio, splice/slice tous les mots, verif perfs
+// REVIEW amélio, splice/slice tous les mots, verif perfs
 handlebars.registerHelper("maxCellChar", function (string) {
   let liste = string.split(' ');
   let lastIndex = liste.length - 1;
@@ -20,7 +20,7 @@ handlebars.registerHelper("maxCellChar", function (string) {
 
 const source = fs.readFileSync(path.resolve(__dirname, 'index.hbs')).toString();
 const template = handlebars.compile(source);
-const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'lists.json')));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'list.json')));
 const result = template(data);
 
 app.get('/', (req, res) => {
